3b42f2ad46e4c3b3b1a724a5ed0295b1c9515397
Evgeniy Demeshko <evgeniy@sixthsummit.io>
2026-03-28 21:04:41 +0100

    feat: initialize server with task management API
    - Add package.json for server dependencies and scripts
    - Create app.js to set up Express server and middleware
    - Implement task status constants
    - Develop taskMockController for CRUD operations on tasks
    - Add date middleware for formatting and parsing due dates
    - Create taskMockModel to manage task data
    - Define taskMockRoutes for API endpoints
    - Set up server entry point in server.js
    - Implement validation for task creation and updates in TaskValidator

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const multer = require('multer');
+    
+const taskMockRoutes = require('./routes/taskMockRoutes');
+
+const { parseDateMiddleware, formatResponseDateMiddleware } = require('./middleware/dateMiddleware');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(multer().any());
+
+app.use(parseDateMiddleware);
+app.use(formatResponseDateMiddleware);
+
+app.use('/api/v1', taskMockRoutes);
+
+module.exports = app;
---
+++ server/src/controllers/taskMockController.js
@@ -0,0 +1,57 @@
+const { tasks } = require('../models/taskMockModel');
+
+const getTasks = (req, res) => {
+    res.json(tasks);
+}
+
+const createTask = (req, res) => {
+    const taskId = tasks.length ? Math.max(...tasks.map(t => t.id)) + 1 : 1;
+    const newTask = {
+        id: taskId,
+        ...req.body
+    }
+
+    tasks.push(newTask);
+
+    res.status(201).json(newTask);
+}
+
+const updateTask = (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const updateTask = { ...req.body };
+
+    const taskIndex = tasks.findIndex(t => t.id === taskId);
+    if (taskIndex !== -1) {
+        tasks[taskIndex] = { id: taskId , ...updateTask};
+        res.json(tasks[taskIndex]);
+    } else {
+        res.status(404).json({ message: 'Завдання не знайдено' });
+    }
+}
+
+const patchTask = (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const updates = req.body;
+
+    const task = tasks.find(task => task.id === taskId);
+    if (task) {
+        Object.assign(task, updates);
+        res.json(task);
+    } else {
+        res.status(404).json({ message: 'Завдання не знайдено' });
+    }
+}
+
+const deleteTask = (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+    if (taskIndex !== -1) {
+        tasks.splice(taskIndex, 1);
+        res.json({ message: `Завдання ${taskId} видалено` });
+    } else {
+        res.status(404).json({ message: 'Завдання не знайдено' });
+    }
+}
+
+module.exports = { getTasks, createTask, updateTask, patchTask, deleteTask };
